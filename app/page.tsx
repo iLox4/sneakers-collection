faa95e91a181b343a98e1dda645babfbb7020f96
iLox4 <ilyaumnovvv@gmail.com>
2023-11-11 09:10:41 +0100

    .gitignore update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return <div><h1>Here will be the content! YEAH!!</h1></div>
+  return <div><h1>Here will be the content!</h1></div>
 }
