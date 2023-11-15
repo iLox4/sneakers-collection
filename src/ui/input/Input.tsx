84d70ce079d5da2ab08210334c9000fc06b4b480
iLox4 <ilyaumnovvv@gmail.com>
2023-11-15 01:52:05 +0100

    update Input

--- src/ui/input/Input.tsx
+++ src/ui/input/Input.tsx
@@ -1,4 +1,4 @@
-import { MouseEventHandler, ReactElement } from "react";
+import { ReactElement, ChangeEvent } from "react";
 
 import styles from "./Input.module.css";
 
@@ -6,34 +6,69 @@ export default function Input({
   isIcon,
   Icon,
   handleInput,
+  placeholder,
   label,
+  type,
+  myValue,
+  isAlert,
 }: {
   isIcon: boolean;
   Icon: () => ReactElement;
-  handleInput: () => void;
+  handleInput: (event: ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
   label: string;
+  type: string;
+  myValue: string | number | undefined;
+  isAlert: boolean;
 }) {
   let content;
+  let inputStyles = `${styles.input}`;
+
+  if (isAlert && (!myValue || myValue === 0)) {
+    inputStyles += ` ${styles.alert}`;
+  }
 
   if (isIcon) {
+    inputStyles += ` ${styles.inputIcon}`;
     content = (
       <div className={styles.inputWrapper}>
         <Icon />
         <input
-          className={`${styles.input} ${styles.inputIcon}`}
+          type={type}
+          className={inputStyles}
           onChange={handleInput}
-          placeholder={label}
+          placeholder={placeholder}
+          value={myValue}
         />
       </div>
     );
   } else {
     content = (
-      <input
-        className={styles.input}
-        onChange={handleInput}
-        placeholder={label}
-      />
+      <>
+        <input
+          type={type}
+          className={inputStyles}
+          onChange={handleInput}
+          placeholder={placeholder}
+          value={myValue}
+        />
+        {isAlert && (!myValue || myValue === 0) && (
+          <label className={styles.alertMessage}>
+            This field cannot be empty
+          </label>
+        )}
+      </>
     );
   }
+
+  if (label) {
+    content = (
+      <>
+        <label className={styles.inputLabel}>{label}</label>
+        {content}
+      </>
+    );
+  }
+
   return content;
 }
