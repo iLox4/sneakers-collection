76a2314969cd591a329343cc3305db01f207ce91
iLox4 <ilyaumnovvv@gmail.com>
2023-11-15 01:51:11 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ const nextConfig = {
     });
     return config;
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/main",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
